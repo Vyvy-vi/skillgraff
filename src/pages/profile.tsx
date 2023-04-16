228b0656f2376b9281f93ae16455af5c9fb5a75d
Vy <vyom.j@protonmail.com>
2023-04-16 10:03:47 +0530

    tst: ...

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import Layout from "../components/layout"
 import SEOHead from "../components/head"
 import { useAuth0, withAuthenticationRequired  } from "@auth0/auth0-react";
-import { Heading, Box, Flex } from "../components/ui"
+import { Heading, Box, Flex, Button } from "../components/ui"
 
 function Login() {
   const { user } = useAuth0();
@@ -14,6 +14,8 @@ function Login() {
         <img src={user.picture} alt="profile image" className="" />
         <Heading as="h3">{user.name}</Heading> 
         <p>Logged in as {user.email}.</p>
+        <Button>Populate profile from LinkedIn</Button>
+        <Button>Populate profile from GitHub</Button>
       </Flex>
       </Box>
     </Layout>
